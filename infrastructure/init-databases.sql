e1443305c49383c5c0c0a9d6c98538781da7c032
Katarina Gojković <kg4775@student.uni-lj.si>
2025-11-30 02:29:52 +0100

    user-service backend logika + keycloak

---
+++ infrastructure/init-databases.sql
@@ -0,0 +1,5 @@
+-- Create Keycloak database
+CREATE DATABASE planify_keycloak OWNER planify ENCODING UTF8;
+
+-- Create additional databases if needed for other services
+-- CREATE DATABASE planify_events OWNER planify ENCODING UTF8;
